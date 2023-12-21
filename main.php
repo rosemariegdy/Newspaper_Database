677112d8ad2a9e98b2f8c167b06abbdbcf598997
Rose Marie Godoy <154398094+rosemariegdy@users.noreply.github.com>
2023-12-20 23:36:59 -0600

    Add files via upload

---
+++ main.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>News Data Report</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="style/bootstrap.css" rel="stylesheet"/>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="main.php">Home</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="data-import.php">Import</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="user.php">user</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="article.php">article</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="author.php">author</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="jumbotron text-center mt-4">
+    <h1 class="display-3">Welcome to Byerly Times!</h1>
+    <p>Greetings, News Explorers! Buckle up as we take you on a thrilling ride through the ever-changing landscape of news.</p>
+    <p>Breaking News: Local Squirrels Organize Annual Nutty Olympics - Acorn Athletes Shine!</p>
+    <p>Launching You Into The Latest Buzz!</p>
+</div>
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-6">
+            <h2>About Us</h2>
+            <p>Discover the world with Byerly Times - your trusted source for exciting and reliable news coverage. Our mission is to provide you with the latest updates and stories that matter.</p>
+        </div>
+        <div class="col-md-6">
+            <h2>Stay Connected</h2>
+            <p>Follow us on social media to stay up-to-date with the hottest news trends. Join the conversation and share your thoughts with the Byerly Times community.</p>
+        </div>
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<footer class="fixed-bottom bg-light pt-3">
+    <p>&copy; 2023 Byerly Times Company, All rights reserved. Potentially Sponsored by Dr. Adam Byerly.</p>
+</footer>
+</body>
+</html>
